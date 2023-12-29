67fbc1471ff7aecb18af417e70afdf4bcfea06bb
Vinh Nguyen <vinhnguyensj01@gmail.com>
2023-12-29 01:10:05 -0800

    made cards for each gas station template

--- App.tsx
+++ App.tsx
@@ -1,117 +1,121 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
+import { Linking } from 'react-native';
+
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+type GreetingProps = {
+  name: string;
+};
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const Header = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+      <Text style={styles.appTitle}>Gas Prices</Text>
+    </View>
+  );
+};
+
+const Card = () => {
+  return (
+    <View style={styles.card}>
+      <Text style={styles.websiteLink} onPress={() => Linking.openURL('https://www.quikstop.com/find-us/generic-store-page/?id=550034')}>QuikStop</Text>
+      <Text style={styles.name}>123 Lane Street 95116</Text>
+      <View style={styles.pricesContainer}>
+        <View style={styles.unleadedContainer}>
+          <Text style={styles.name}>Unleaded</Text>
+          <Text style={styles.name}>$3.49</Text>
+        </View>
+        <View style={styles.leadedContainer}>
+          <Text style={styles.name}>Leaded</Text>
+          <Text style={styles.name}>$6.49</Text>
+        </View>
+      </View>
+
+    </View>
+  );
+};
+
+function App() {
+  return (
+    <View style = {{flex: 1}}>
+      <Header />
+      <ScrollView style = {{flex: 1}}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </ScrollView>
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  appTitle: {
+    fontWeight: 'bold',
+    fontSize: 40,
+    marginLeft: 15,
+    marginTop: 5,
+    color: 'black',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  card: {
+    flex: 1,
+    justifyContent: 'space-between',
+    backgroundColor: '#fff',
+    padding: 10,
+    margin: 10,
+    borderWidth: 4,
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+    borderBottomLeftRadius: 5,
+    borderBottomRightRadius: 5,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  name: {
+    fontSize: 25,
+    color: 'black',
   },
-  highlight: {
-    fontWeight: '700',
+  pricesContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    fontSize: 15,
+    color: 'black',
+
+  },
+  unleadedContainer: {
+    flexGrow: 1,
+    flexBasis: 100,
+    padding: 10,
+    margin: 4,
+    borderWidth: 4,
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+    borderBottomLeftRadius: 5,
+    borderBottomRightRadius: 5,
+  },
+  leadedContainer: {
+    flexGrow: 1,
+    flexBasis: 100,
+    padding: 10,
+    margin: 4,
+    borderWidth: 4,
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+    borderBottomLeftRadius: 5,
+    borderBottomRightRadius: 5,
+  },
+  websiteLink: {
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 25,
   },
 });
 
